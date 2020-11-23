53d5a6e212b34f1e648e7c3e214b33f6047b3833
Wazen Shbair <wazen.shbair@gmail.com>
2020-11-23 20:32:47 +0100

    Update load_test.js

--- basic_payid_tests/load_test.js
+++ basic_payid_tests/load_test.js
@@ -7,9 +7,9 @@ export let errorRate = new Rate("errors");
 const host='127.0.0.1'
 export let options = {
     stages: [
-        { duration: '5m', target: 500 }, // simulate ramp-up of traffic from 1 to 100 users over 5 minutes.
-        { duration: '10m', target: 500 }, // stay at 100 users for 10 minutes
-        { duration: '5m', target: 0 }, // ramp-down to 0 users
+        { duration: '60s', target: 1000 }, // simulate ramp-up of traffic from 1 to 1000 users over 60 secconds.
+        { duration: '120s', target:2000 }, // stay at 2000 users for 120 seconds 
+        { duration: '10s', target: 0 }, // ramp-down to 0 users
       ]
   };
 
