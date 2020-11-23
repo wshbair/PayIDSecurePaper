35d244753078651ccd7e14eb16ba4ca2c96c60d5
Wazen Shbair <wazen.shbair@gmail.com>
2020-11-23 20:34:02 +0100

    Update stress_test.js

--- basic_payid_tests/stress_test.js
+++ basic_payid_tests/stress_test.js
@@ -7,15 +7,15 @@ export let errorRate = new Rate("errors");
 const host='127.0.0.1'
 export let options = {
     stages: [
-        { duration: '2m', target: 100 }, // below normal load
-        { duration: '5m', target: 100 },
-        { duration: '2m', target: 200 }, // normal load
-        { duration: '5m', target: 200 },
-        { duration: '2m', target: 300 }, // around the breaking point
-        { duration: '5m', target: 300 },
-        { duration: '2m', target: 400 }, // beyond the breaking point
-        { duration: '5m', target: 400 },
-        { duration: '10m', target: 0 }, // scale down. Recovery stage.
+        { duration: '20s', target: 100 }, // below normal load
+        { duration: '50s', target: 100 },
+        { duration: '60s', target: 200 }, // normal load
+        { duration: '50s', target: 200 },
+        { duration: '20s', target: 300 }, // around the breaking point
+        { duration: '50s', target: 300 },
+        { duration: '20s', target: 400 }, // beyond the breaking point
+        { duration: '50s', target: 400 },
+        { duration: '10s', target: 0 }, // scale down. Recovery stage.
       ]
   };
 
