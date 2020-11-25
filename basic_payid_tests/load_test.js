11a660a23abd91e0ba5d1211176d8a8562a18835
Wazen Shbair <wazen.shbair@gmail.com>
2020-11-25 18:15:38 +0100

    Update load_test.js

--- basic_payid_tests/load_test.js
+++ basic_payid_tests/load_test.js
@@ -4,7 +4,7 @@ import { Rate } from "k6/metrics";
 import crypto from "k6/crypto";
 
 export let errorRate = new Rate("errors");
-const host='127.0.0.1'
+const host='petitprince-6.luxembourg.grid5000.fr'
 export let options = {
     stages: [
         { duration: '5m', target: 100 }, // simulate ramp-up of traffic from 1 to 100 users over 5 minutes
